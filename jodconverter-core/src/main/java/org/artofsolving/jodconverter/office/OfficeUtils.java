c75b056932903414945e61a3bcf6c96a5fe38618
Eric Tallman <erictallman@gmail.com>
2014-01-08 14:21:42 -0500

    fix inconsistent whitespacing

--- jodconverter-core/src/main/java/org/artofsolving/jodconverter/office/OfficeUtils.java
+++ jodconverter-core/src/main/java/org/artofsolving/jodconverter/office/OfficeUtils.java
@@ -24,146 +24,147 @@ import com.sun.star.uno.UnoRuntime;
 
 public class OfficeUtils {
 
-    public static final String SERVICE_DESKTOP = "com.sun.star.frame.Desktop";
+	public static final String SERVICE_DESKTOP = "com.sun.star.frame.Desktop";
 
-    private OfficeUtils() {
-        throw new AssertionError("utility class must not be instantiated");
-    }
+	private OfficeUtils() {
+		throw new AssertionError("utility class must not be instantiated");
+	}
 
-    public static <T> T cast(Class<T> type, Object object) {
-        return (T) UnoRuntime.queryInterface(type, object);
-    }
+	public static <T> T cast(Class<T> type, Object object) {
+		return (T) UnoRuntime.queryInterface(type, object);
+	}
 
-    public static PropertyValue property(String name, Object value) {
-        PropertyValue propertyValue = new PropertyValue();
-        propertyValue.Name = name;
-        propertyValue.Value = value;
-        return propertyValue;
-    }
+	public static PropertyValue property(String name, Object value) {
+		PropertyValue propertyValue = new PropertyValue();
+		propertyValue.Name = name;
+		propertyValue.Value = value;
+		return propertyValue;
+	}
 
-    @SuppressWarnings("unchecked")
-    public static PropertyValue[] toUnoProperties(Map<String,?> properties) {
-        PropertyValue[] propertyValues = new PropertyValue[properties.size()];
-        int i = 0;
-        for (Map.Entry<String,?> entry : properties.entrySet()) {
-            Object value = entry.getValue();
-            if (value instanceof Map) {
-                Map<String,Object> subProperties = (Map<String,Object>) value;
-                value = toUnoProperties(subProperties);
-            }
-            propertyValues[i++] = property((String) entry.getKey(), value);
-        }
-        return propertyValues;
-    }
+	@SuppressWarnings("unchecked")
+	public static PropertyValue[] toUnoProperties(Map<String,?> properties) {
+		PropertyValue[] propertyValues = new PropertyValue[properties.size()];
+		int i = 0;
+		for (Map.Entry<String,?> entry : properties.entrySet()) {
+			Object value = entry.getValue();
+			if (value instanceof Map) {
+				Map<String,Object> subProperties = (Map<String,Object>) value;
+				value = toUnoProperties(subProperties);
+			}
+			propertyValues[i++] = property((String) entry.getKey(), value);
+		}
+		return propertyValues;
+	}
 
-    public static String toUrl(File file) {
-        String path = file.toURI().getRawPath();
-        String url = path.startsWith("//") ? "file:" + path : "file://" + path;
-        return url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
-    }
+	public static String toUrl(File file) {
+		String path = file.toURI().getRawPath();
+		String url = path.startsWith("//") ? "file:" + path : "file://" + path;
+		return url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
+	}
 
-    public static File getDefaultOfficeHome() {
-        if (System.getProperty("office.home") != null) {
-            return new File(System.getProperty("office.home"));
-        }
-        if (PlatformUtils.isWindows()) {
-            // %ProgramFiles(x86)% on 64-bit machines; %ProgramFiles% on 32-bit ones
-            String programFiles = System.getenv("ProgramFiles(x86)");
-            if (programFiles == null) {
-                programFiles = System.getenv("ProgramFiles");
-            }
-            return findOfficeHome(
-                programFiles + File.separator + "OpenOffice.org",
-                programFiles + File.separator + "OpenOffice.org 3",
-                programFiles + File.separator + "LibreOffice",
-                programFiles + File.separator + "LibreOffice 3",
-                programFiles + File.separator + "LibreOffice 4"
-            );
-        } else if (PlatformUtils.isMac()) {
-            File officeHome = findOfficeHome(
-                "/Applications/OpenOffice.org.app/Contents",
-                "/Applications/LibreOffice.app/Contents"
-            );
+	public static File getDefaultOfficeHome() {
+		if (System.getProperty("office.home") != null) {
+			return new File(System.getProperty("office.home"));
+		}
+		if (PlatformUtils.isWindows()) {
+			// %ProgramFiles(x86)% on 64-bit machines; %ProgramFiles% on 32-bit ones
+			String programFiles = System.getenv("ProgramFiles(x86)");
+			if (programFiles == null) {
+				programFiles = System.getenv("ProgramFiles");
+			}
+			return findOfficeHome(
+				programFiles + File.separator + "OpenOffice.org",
+				programFiles + File.separator + "OpenOffice.org 3",
+				programFiles + File.separator + "LibreOffice",
+				programFiles + File.separator + "LibreOffice 3",
+				programFiles + File.separator + "LibreOffice 4"
+			);
+		} else if (PlatformUtils.isMac()) {
+			File officeHome = findOfficeHome(
+				"/Applications/OpenOffice.org.app/Contents",
+				"/Applications/LibreOffice.app/Contents"
+			);
 
-            // Try with locate
-            if (officeHome == null) {
-            	officeHome = locateOfficeHome();
-            }
-            return officeHome;
-        } else {
-            // Linux or other *nix variants
-            File officeHome = findOfficeHome(
-                "/opt/openoffice",
-                "/opt/libreoffice",
-                "/usr/bin/openoffice",
-                "/usr/bin/libreoffice",
-                "/usr/lib/openoffice",
-                "/usr/lib/libreoffice",
-                "/usr/local/lib/openoffice",
-                "/usr/local/lib/libreoffice"
-            );
+			// Try with locate
+			if (officeHome == null) {
+				officeHome = locateOfficeHome();
+			}
+			return officeHome;
+		} else {
+			// Linux or other *nix variants
+			File officeHome = findOfficeHome(
+				"/opt/openoffice",
+				"/opt/libreoffice",
+				"/usr/bin/openoffice",
+				"/usr/bin/libreoffice",
+				"/usr/lib/openoffice",
+				"/usr/lib/libreoffice",
+				"/usr/local/lib/openoffice",
+				"/usr/local/lib/libreoffice"
+			);
 
-            // Try with locate
-            if (officeHome == null) {
-            	officeHome = locateOfficeHome();
-            }
-            return officeHome;
-        }
-    }
+			// Try with locate
+			if (officeHome == null) {
+				officeHome = locateOfficeHome();
+			}
+			return officeHome;
+		}
+	}
 
-    private static File locateOfficeHome() {
+	private static File locateOfficeHome() {
 
-        String s = null;
-        String cmd = "locate soffice.bin";
-        try {
-        	Process p = Runtime.getRuntime().exec(cmd);
-        	int i = p.waitFor();
-        	if (i == 0){
-        		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String s = null;
+		String cmd = "locate soffice.bin";
+		try {
+			Process p = Runtime.getRuntime().exec(cmd);
+			int i = p.waitFor();
+			if (i == 0) {
+				BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-        		// read the output from the command
-        		while ((s = stdInput.readLine()) != null) {
-        			File f = new File(s);
-        			if (f.exists() && f.getName().equalsIgnoreCase("soffice.bin") && f.getParentFile() != null && f.getParentFile().getParentFile() != null) {
-        				return f.getParentFile().getParentFile();
-        			}
-        		}
-        	}
-        	else {
-        		BufferedReader stdErr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-        		// read the error output from the command
-        		while ((s = stdErr.readLine()) != null) {
-        		}
-        	}
-        } catch (Throwable th) {
-        }
-        return null;
-    }
+				// read the output from the command
+				while ((s = stdInput.readLine()) != null) {
+					File f = new File(s);
+					if (f.exists() && f.getName().equalsIgnoreCase("soffice.bin") && f.getParentFile() != null && f.getParentFile().getParentFile() != null) {
+						return f.getParentFile().getParentFile();
+					}
+				}
+			}
+			else {
+				BufferedReader stdErr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+				// read the error output from the command
+				while ((s = stdErr.readLine()) != null) {
+				}
+			}
+		} catch (Throwable th) {
+		}
+		return null;
+	}
 
-    private static File findOfficeHome(String... knownPaths) {
-        for (String path : knownPaths) {
-            File home = new File(path);
-            File containerPath = home.getParentFile();
-            if (containerPath != null && containerPath.isDirectory()) {
-            	for (File subDir : containerPath.listFiles()) {
-            		if (subDir.isDirectory() && subDir.getName().startsWith(home.getName())) {
-                        if (getOfficeExecutable(subDir).isFile()) {
-                            return home;
-                        }
-            		}
-            	}
-            }
-        }
-        return null;
-    }
+	private static File findOfficeHome(String... knownPaths) {
+		for (String path : knownPaths) {
+			File home = new File(path);
+			File containerPath = home.getParentFile();
+			if (containerPath != null && containerPath.isDirectory()) {
+				for (File subDir : containerPath.listFiles()) {
+					if (subDir.isDirectory() && subDir.getName().startsWith(home.getName())) {
+						if (getOfficeExecutable(subDir).isFile()) {
+							return home;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
 
-    public static File getOfficeExecutable(File officeHome) {
-        if (PlatformUtils.isMac()) {
-            return new File(officeHome, "MacOS/soffice.bin");
-        } else if (PlatformUtils.isWindows()) {
+	public static File getOfficeExecutable(File officeHome) {
+		if (PlatformUtils.isMac()) {
+			return new File(officeHome, "MacOS/soffice.bin");
+		} else if (PlatformUtils.isWindows()) {
 			return new File(officeHome, "program/soffice.exe");
-		}else {
-            return new File(officeHome, "program/soffice.bin");
-        }
-    }
+		} else {
+			return new File(officeHome, "program/soffice.bin");
+		}
+	}
+
 }
